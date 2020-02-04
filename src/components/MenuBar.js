5a591537a1896fd32a79b02e5c300d7542ab6a2c
Miapan <miapanyuewei@gmail.com>
2020-02-03 22:08:54 -0800

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,19 +15,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className="item active" id="profile" onClick={() => props.onSwitchItem(props.profile)}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className="item" id="photo" onClick={() => props.onSwitchItem(props.photos)}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className="item" id="cocktail" onClick={() => props.onSwitchItem(props.cocktails)}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className="item" id="pokemon" onClick={() => props.onSwitchItem(props.pokemon)}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
